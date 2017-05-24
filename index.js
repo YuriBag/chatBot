83216ca3be45dc17561dd3a33061f0237a371375
Yuri <yuridg44@gmail.com>
2017-05-24 16:08:48 +0400

    Yuri
    Chat bot

---
+++ index.js
@@ -0,0 +1,22 @@
+const VK = require('vk-io');
+const vk = new VK({token:"74e06880e1dc65f8817f7687a2163d027cfb70a70451e524d2f522a54f47e21415c1fb1ed2c1169b2bd21"});
+
+
+
+
+vk.longpoll.start()
+    .then(() => {
+    console.log('Long Poll запущен');
+})
+.catch((error) => {
+    console.error(error);
+});
+vk.longpoll.on("message",(msg)=>{
+    if(-1!==msg.flags.indexOf('outbox')){
+        return;
+    }
+    //console.log(msg.text);
+    msg.send("lav em");
+});
+
+
